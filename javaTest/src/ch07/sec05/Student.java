652f4279cf8df57e63acd40dae0ad72e57be95dc
bjkim247 <bjkim247@gmail.com>
2023-09-27 10:42:48 +0900

    436쪽 실습문제 5번

--- javaTest/src/ch07/sec05/Student.java
+++ javaTest/src/ch07/sec05/Student.java
@@ -3,42 +3,65 @@ package ch07.sec05;
 import java.util.Objects;
 
 public class Student {
-	//멤버변수
+	private String name;
+	private String major;
 	private int id;
-	private String tel;
-	//생성자(디생,매생)
-	
+	private double gpa;
+
+
 	public Student() {
-		this(0,null);
+		this(null, null, 0, 0.0);
 	}
-	public Student(int id, String tel) {
-	super();
-	this.id = id; 
-	this.tel = tel; 
+
+	public Student(String name, int id) {
+		this(name, null, id, 0.0);
 	}
-	//멤버함수
+
+	public Student(String name, String major, int id, double gpa) {
+		super();
+		this.name = name;
+		this.major = major;
+		this.id = id;
+		this.gpa = gpa;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
 	public int getId() {
 		return id;
 	}
-	public String getTel() {
-		return tel;
+
+	public double getGpa() {
+		return gpa;
 	}
-	@Override
-	public boolean equals(Object obj) {
-		Student stu = (Student)obj;
-		if(this.id == stu.id && this.tel.equals(stu.tel)) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(id+tel);
-	}
+
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", tel=" + tel + "]";
+		return "[name=" + name + ", major=" + major + ", id=" + id + ", gpa=" + gpa + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name, this.id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Student01 stu = null; 
+		if(!(obj instanceof Student01)) {
+			return false; 
+		}
+		stu = (Student01)obj;
+		if(this.name.equals(stu.name) && this.id == stu.id) {
+			return true; 
+		}
+		return false;
 	}
 
 }
